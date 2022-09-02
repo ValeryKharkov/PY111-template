a565127701354ba453d8ad5122b073b76383ef8f
ValeryKharkov <62515966+ValeryKharkov@users.noreply.github.com>
2022-09-02 09:27:00 +0300

    Initial commit

---
+++ Tasks/f0_binary_search_tree.py
@@ -0,0 +1,50 @@
+"""
+You can do it either with networkx ('cause tree is a graph)
+or with dicts (smth like {'key': 0, value: 123, 'left': {...}, 'right':{...}})
+"""
+
+from typing import Any, Optional, Tuple
+# import networkx as nx
+
+
+def insert(key: int, value: Any) -> None:
+    """
+    Insert (key, value) pair to binary search tree
+
+    :param key: key from pair (key is used for positioning node in the tree)
+    :param value: value associated with key
+    :return: None
+    """
+    print(key, value)
+    return None
+
+
+def remove(key: int) -> Optional[Tuple[int, Any]]:
+    """
+    Remove key and associated value from the BST if exists
+
+    :param key: key to be removed
+    :return: deleted (key, value) pair or None
+    """
+    print(key)
+    return None
+
+
+def find(key: int) -> Optional[Any]:
+    """
+    Find value by given key in the BST
+
+    :param key: key for search in the BST
+    :return: value associated with the corresponding key
+    """
+    print(key)
+    return None
+
+
+def clear() -> None:
+    """
+    Clear the tree
+
+    :return: None
+    """
+    return None
---
+++ Tasks/g2_quick_sort.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def sort(container: List[int]) -> List[int]:
+    """
+    Sort input container with quick sort
+
+    :param container: container of elements to be sorted
+    :return: container sorted in ascending order
+    """
+    return container
